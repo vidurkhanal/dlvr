c4bd814cdd3baa779ae09ae8b8764f4ccec6d280
vidurkhanal <vkhanal@caldwell.edu>
2022-07-21 20:18:58 -0400

    shhhhgit

--- auth-and-users/main.go
+++ auth-and-users/main.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-	repo.Connect("postgres://lkbtxmfjlzhkaw:9f133ef2f7cfc968126e983141ce623e88f941ea68205eea2c6578374a1bfe44@ec2-54-87-179-4.compute-1.amazonaws.com:5432/da75tbdvv6c9p6")
+	repo.Connect("")
 	repo.Migrate()
 }
