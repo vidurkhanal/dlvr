7fb2cdd65d9f1fed847a84b57a443ae77624cf96
vidurkhanal <vkhanal@caldwell.edu>
2022-07-23 23:35:08 -0400

    LOGGER AND NODEMON ADDED

--- gateway/src/server.ts
+++ gateway/src/server.ts
@@ -7,18 +7,24 @@ import { HelloResolver } from "./resolvers/HelloResolver";
 
 const main = async () => {
   const app = Express();
+
+  //   MIDDLEWARES
   app.use(cors({ credentials: true }));
 
+  //   BUIDLING TYPEGRAPHQL SCHEMA
   const schema = await buildSchema({
     resolvers: [HelloResolver],
     validate: false,
   });
 
-  const apollo = new ApolloServer({ schema });
+  // APOLLO STUFFS
+  const apollo = new ApolloServer({
+    schema,
+  });
   await apollo.start();
-
   apollo.applyMiddleware({ app });
 
+  // START THE TING
   app.listen({ port: 5050 }, () => {
     console.log(
       "GRAPHQL SERVER IS ACTIVE ON http:localhost:5050" + apollo.graphqlPath
